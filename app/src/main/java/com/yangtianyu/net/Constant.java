f1849a3a853cbf22ea0f71841edb497d7648c177
yangtianyu <810255869@qq.com>
2017-12-26 22:46:31 +0800

    1、替换API_KEY

--- app/src/main/java/com/yangtianyu/net/Constant.java
+++ app/src/main/java/com/yangtianyu/net/Constant.java
@@ -5,7 +5,7 @@ package com.yangtianyu.net;
  */
 
 public class Constant {
-    public static final String API_KEY = "3e8102245ef380f1c097fb6e992f0a54";
+    public static final String API_KEY = "YOUR_API_KEY";
     public static final String LANGUAGE = "zh-CN";
     public static final String REGION = "CN";
     public static final String MOVIE_ID = "movie_id";
